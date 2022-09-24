82fbc080c3e1a6dd0050dbb709256d6652fd4b02
Koding Dev <kodingdev@pm.me>
2022-09-24 23:16:02 +1000

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+<!--suppress CheckImageSize, HtmlDeprecatedAttribute -->
+<img align="right" width="80" height="80" src=".github/yawning-abyss.png" alt="The Yawning Abyss">
+
+# Yoodle
+
+⚒️ Get it cause it sounds like "Util" but it's "Yoodle" isn't that funny?
+
+## Features
+
+* All of them.
+* (Currently none)
---
+++ src/main/kotlin/org/britmoji/yoodle/App.kt
@@ -0,0 +1,36 @@
+package org.britmoji.yoodle
+
+import com.kotlindiscord.kord.extensions.ExtensibleBot
+import mu.KotlinLogging
+import org.britmoji.yoodle.config.config
+import org.britmoji.yoodle.util.Colors
+import org.britmoji.yoodle.util.feedback
+
+private val logger = KotlinLogging.logger {  }
+
+suspend fun main() {
+    // Warn for missing config
+    if (config.bot.token == "INSERT_TOKEN_HERE") {
+        return logger.error { "Please configure the bot before running it. A default config has been created." }
+    }
+
+    // Create the bot
+    val bot = ExtensibleBot(config.bot.token) {
+        applicationCommands {
+            enabled = true
+            defaultGuild(config.bot.guildId)
+        }
+
+        errorResponse { message, type ->
+            feedback(type.error.message ?: message) {
+                color = Colors.ERROR
+            }
+        }
+
+        presence {
+            watching("you sleep")
+        }
+    }
+
+    bot.start()
+}
